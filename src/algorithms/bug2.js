8ecdd1755de56b21820d9af783b30e8c7ac729eb
prism7ai <71543802+prism7ai@users.noreply.github.com>
2025-06-12 14:03:34 +0530

    Update bug2.js

--- src/algorithms/bug2.js
+++ src/algorithms/bug2.js
@@ -1,10 +1,4 @@
 export function bug2(start, goal, obstacles, size) {
-  console.log("start", start); // Add this at the top of astar and bug2
-
-if (!Array.isArray(start)) {
-  throw new Error("Start must be an array like [x, y]");
-}
-
   // Fake curved path to simulate bug2 logic
   let path = [];
   let [x, y] = start;
