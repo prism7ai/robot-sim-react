1b14ea904e3908220ccd5768ea207353d35703e2
prism7ai <71543802+prism7ai@users.noreply.github.com>
2025-06-12 14:03:05 +0530

    Update astar.js

--- src/algorithms/astar.js
+++ src/algorithms/astar.js
@@ -1,10 +1,4 @@
 export function astar(start, goal, obstacles, size) {
-  console.log("start", start); // Add this at the top of astar and bug2
-
-if (!Array.isArray(start)) {
-  throw new Error("Start must be an array like [x, y]");
-}
-
   // Simple dummy straight line path (replace with real A* later)
   let path = [];
   let [x, y] = start;
